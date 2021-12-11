a249b82eb5a5166c5e2a8608ed2dce5c3970aee6
josephkelly123 <x00153381@mytudublin.ie>
2021-12-11 20:55:28 +0000

    commit on master

--- dropdown.js
+++ dropdown.js
@@ -18,103 +18,69 @@ $(document).ready(function () {
         });
     }).change();
 });
+
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
-var modal = document.getElementById("myModal1");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn1");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close1")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
+
+var modal2 = document.getElementById("myModal2");
+
+
+var btn2 = document.getElementById("myBtn2");
+
+var span2 = document.getElementsByClassName("close2")[0];
+
+btn2.onclick = function () {
+    modal2.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+span2.onclick = function () {
+    modal2.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
 
-var modal = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close2")[0];
+var span3 = document.getElementsByClassName("close3")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
+btn3.onclick = function () {
+    modal3.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+span3.onclick = function () {
+    modal3.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-var modal = document.getElementById("myModal3");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn3");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close3")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal3) {
+        modal3.style.display = "none";
     }
 }
 
@@ -122,15 +88,14 @@ window.onclick = function (event) {
 
 $(function () {
 
-    // contact form animations
     $('#contact').click(function () {
         $('#contactForm').fadeToggle();
     })
     $(document).mouseup(function (e) {
         var container = $("#contactForm");
 
-        if (!container.is(e.target) // if the target of the click isn't the container...
-            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        if (!container.is(e.target) 
+            && container.has(e.target).length === 0) 
         {
             container.fadeOut();
         }
@@ -140,5 +105,10 @@ $(function () {
 
 function myhiddendiv() {
     var T = document.getElementById("testing-hidden");
-    T.style.display = "block";  // <-- Set it to block
+    T.style.display = "block";  
+}
+
+function myhiddendiv1() {
+    var T = document.getElementById("testing-hidden1");
+    T.style.display = "block";
 }
